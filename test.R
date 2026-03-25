6d57d31cc78f11b820bb0e9c05151f3e4ce541fd
rijpma <auke.rijpma@gmail.com>
2026-03-25 10:17:36 +0100

    clean up tests

--- test.R
+++ test.R
@@ -1,7 +1,15 @@
 library("ineq")     # inequality measures and example data
 library("EnvStats") # for estimation of censored distributions
 
-source("impute.R") 
+source("impute.R")
+
+calc_metrics = function(y) {
+    list(
+        gini = ineq::Gini(y),
+        mean_log = mean(log(y)),
+        sd_log = sd(log(y))
+    )
+}
 
 # to test we manually truncate the Ilocos income data from the ineq package
 data(Ilocos, package = "ineq")
@@ -13,36 +21,28 @@ n_missing = sum(Ilocos$income < 40e3)
 # truncate distribution
 y_trunc = Ilocos$income[Ilocos$income >= 40e3]
 
+# impute distribution
+imputed = sample_from_lognormal(y_trunc, n_missing)
+
 # a look at the two distributions
-par(mfrow = c(1, 2))
+par(mfrow = c(1, 3))
 xr = log(range(Ilocos$income))
 hist(log(Ilocos$income), xlim = xr, breaks = 20)
 hist(log(y_trunc), xlim = xr, breaks = 20)
+hist(log(imputed), xlim = xr, breaks = 20)
+# procedure does not quite capture the skew of the original
 
 # the unbiased estimates
-ineq::Gini(Ilocos$income)
-mean(log(Ilocos$income))
-sd(log(Ilocos$income))
+calc_metrics(Ilocos$income)
 
 # the biased estimates due to truncation
-ineq::Gini(y_trunc)
-mean(log(y_trunc))
-sd(log(y_trunc))
+calc_metrics(y_trunc)
 
-# estimate_lnorm() uses EnvStats::enormCensored() to get unbiased mean and sd
-# for the trunc. lognormal
+# the estimated parameters
 estimate_lnorm(y_trunc, n_missing)
 
-# impute_from_sd() uses the sd from the lognormal to estimate the Gini
+# impute by sampling lognormal
+calc_metrics(imputed)
+
+# impute gini directly
 impute_from_sd(y_trunc, n_missing)
-
-# sample_from_lognormal() uses the EnvStats::rnormTrunc() for random
-# generation for the truncated normal distribution, which can then be used to
-# calculate the Gini on
-resampled = sample_from_lognormal(y_trunc, n_missing)
-ineq::Gini(resampled)
-mean(log(resampled))
-sd(log(resampled))
-
-# impute_from_subsistence fills in the missing observations with an estimate
-# of subsistence income to calculate the Gini. Not implemented yet
