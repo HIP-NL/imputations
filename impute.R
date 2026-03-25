0fc6e0662dc7212065e94eaa88624c5abdd2feaf
rijpma <auke.rijpma@gmail.com>
2026-03-25 10:06:00 +0100

    Imputations use lnorm rather than norm

--- impute.R
+++ impute.R
@@ -4,18 +4,23 @@ estimate_lnorm = function(y, n_missing){
     #   y: the untransformed data
     #   n_missing: the expected number of missing observations
     censored = c(
-        rep(FALSE, length(y)), 
+        rep(FALSE, length(y)),
         rep(TRUE, n_missing)
     )
-    y = c(
-        y, 
+
+    # construct full vector with censored values. Censored values need to be the number to the left of which the true value is
+    y_with_censored = c(
+        y,
         rep(min(y, na.rm = TRUE), n_missing)
     )
-    y = log(y)
-    estimates = EnvStats::enormCensored(y, censored = censored, censoring.side = "left")
+
+    estimates = EnvStats::elnormCensored(
+        y_with_censored,
+        censored = censored,
+        censoring.side = "left")
     return(list(
-        log_mean = estimates$parameters["mean"], 
-        log_sd = estimates$parameters["sd"]))
+        log_mean = estimates$parameters["meanlog"],
+        log_sd = estimates$parameters["sdlog"]))
 }
 
 impute_from_sd = function(y, n_missing){
@@ -28,32 +33,34 @@ impute_from_sd = function(y, n_missing){
     sd_y = parameters$log_sd
     gini = 2*pnorm(sd_y / sqrt(2)) - 1
 
-    return(gini)
+    return(
+        setNames(gini, "gini")
+    )
 }
 
-sample_from_lognormal = function(y, n_missing, min = 0){
+sample_from_lognormal = function(y, n_missing, min_val = 0){
     # fill NAs with resample from trunc lnorm
     # expects
     #   y: the untransformed data
     #   n_missing: the expected number of missing observations
 
-    
+
     parameters = estimate_lnorm(y, n_missing)
-    
-    imputations = rnormTrunc(
-        n = n_missing, 
-        mean = parameters$log_mean, 
-        sd = parameters$log_sd, 
-        min = log(min),
+
+    imputations = rlnormTrunc(
+        n = n_missing,
+        mean = parameters$log_mean,
+        sd = parameters$log_sd,
+        min = log(min_val),
         max = min(log(y)))
 
     out = c(y, exp(imputations))
-    
+
     return(out)
 }
 
 impute_from_subsistence = function(y, n_missing, y_subsistence){
-    # fill NAs with subsistence incomes from X
+    # fill NAs with some fixed (subsistence) incomes
     # tbd
      out = c(y, rep(y_subsistence, times = n_missing))
 
